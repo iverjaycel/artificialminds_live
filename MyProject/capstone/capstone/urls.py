9f47875437b41461a11a656cb2f3a77986326135
Iver Punayan <53965169+iverjaycel@users.noreply.github.com>
2022-04-14 12:12:53 +0800

    Update urls.py

--- MyProject/capstone/capstone/urls.py
+++ MyProject/capstone/capstone/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sample.urls', namespace='sample')),
-
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
